MotorolaMobilityLLC/kernel-msm

Use log_wakeup_reason() to log wakeup reason
Instead of using printk to log the wakeup reason, use
log_wakeup_reason() API. This function not only logs the
wakeup reason but also exposes the last wakeup reason to
userspace via "/sys/kernel/wakeup_reasons/last_wakeup_reason"

Android batterystats reads the last wakeup reason from this
sysfs and logs in the bug report.

Change-Id: Ic5c667b3d0838e4d514d2dea44eb88f8a635a1e4
Signed-off-by: Ravi Chebolu <arc095@motorola.com>
Reviewed-on: http://gerrit.mot.com/842863
SLTApproved: Slta Waiver <sltawvr@motorola.com>
SME-Granted: SME Approvals Granted
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -30,6 +30,7 @@
 #include <linux/reboot.h>
 #include <linux/irqchip/msm-mpm-irq.h>
 #include <linux/syscore_ops.h>
+#include <linux/wakeup_reason.h>
 #include "../core.h"
 #include "../pinconf.h"
 #include "pinctrl-msm.h"
@@ -965,8 +966,7 @@ static void msm_pinctrl_resume(void)
 			else if (desc->name)
 				name = desc->name;
 
-			pr_warn("%s: IRQ %d (gpio %d) triggered %s\n",
-				__func__, irq_pin, i, name);
+			log_wakeup_reason(irq_pin);
 		}
 	}
 }

LineageOS/android_kernel_motorola_msm8996

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -30,6 +30,7 @@
 #include <linux/syscore_ops.h>
 #include <linux/reboot.h>
 #include <linux/irqchip/msm-mpm-irq.h>
+#include <linux/wakeup_reason.h>
 #include "../core.h"
 #include "../pinconf.h"
 #include "pinctrl-msm.h"
@@ -967,7 +968,7 @@ static void msm_pinctrl_resume(void)
 			else if (desc->action && desc->action->name)
 				name = desc->action->name;
 
-			pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+			log_wakeup_reason(irq);
 		}
 	}
 	spin_unlock_irqrestore(&pctrl->lock, flags);